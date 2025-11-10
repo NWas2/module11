9795ea0d965b1daf528f2d0d041c19e852809f53
Najya <designd.bynajya@gmail.com>
2025-11-10 18:54:14 -0500

    Update site

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,15 @@ import Contact from '../views/Contact.vue';
 import NotFound from '../views/NotFound.vue';
 
 const routes = [
-  { path: '/', redirect: '/about' },
-  { path: '/about', component: About },
-  { path: '/quotes', component: RandomQuotes },
+  { path: '/', component: About },
+  { path: '/random-quotes', component: RandomQuotes },
   { path: '/contact', component: Contact },
-  { path: '/:pathMatch(.*)*', component: NotFound }
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
 });
 
 export default router;
